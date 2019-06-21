f0c34897d4eff13c28801a470b6f68d5cd79436b
Manuel Fernandez <manferlo81@gmail.com>
2019-06-21 14:58:18 +0700

    added tests

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const { main } = require("./package.json");
+
+const CI = process.env.CI;
+
+module.exports = {
+
+  testEnvironment: "node",
+  browser: false,
+
+  cacheDirectory: ".cache/jest",
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    main,
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: [
+    CI ? "json" : "lcov",
+    "text",
+    "text-summary",
+  ],
+  coverageThreshold: null,
+
+  verbose: true,
+
+};
---
+++ test/classes.test.js
@@ -0,0 +1,75 @@
+const { classes, fromArray, fromObj } = require("..");
+
+test("should return classname from classname", () => {
+
+  const classname = "class1 class2";
+  const result = classes(classname);
+
+  expect(result).toBe(classname);
+
+});
+
+test("should return classname from multiple classnames", () => {
+
+  const classname1 = "class1";
+  const classname2 = "class2";
+  const result = classes(classname1, classname2);
+
+  expect(result).toBe(`${classname1} ${classname2}`);
+
+});
+
+test("should return empty classname from empty string", () => {
+
+  const classname = "";
+  const result = classes(classname);
+
+  expect(result).toBe(classname);
+
+});
+
+test("should return classname from array", () => {
+
+  const classArray = [
+    "class1",
+    { "class2 class3": true, class4: false },
+    ["class5", "class6"],
+    { class3: false },
+  ];
+  const result = classes(classArray);
+
+  expect(result).toBe("class1 class2 class5 class6");
+
+});
+
+test("should return classname from array using fromArray", () => {
+
+  const classArray = [
+    "class1",
+    { "class2 class3": true, class4: false },
+    ["class5", "class6"],
+    { class3: false },
+  ];
+  const result = fromArray(classArray);
+
+  expect(result).toBe("class1 class2 class5 class6");
+
+});
+
+test("should return classname from object", () => {
+
+  const classObj = { class1: false, "class2 class3": true, class2: false };
+  const result = classes(classObj);
+
+  expect(result).toBe("class3");
+
+});
+
+test("should return classname from object using fromObj", () => {
+
+  const classObj = { class1: false, "class2 class3": true, class2: false };
+  const result = fromObj(classObj);
+
+  expect(result).toBe("class3");
+
+});
