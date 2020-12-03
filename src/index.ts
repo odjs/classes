0a36527b5c1b281cd5ee59d644ac81571d1617cd
Manuel Fernández <manferlo81@gmail.com>
2020-12-03 13:21:56 +0700

    chore: Code cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,60 +2,97 @@ type IsClassPresent = classes.IsClassPresent;
 type NormalizedClassObject = classes.NormalizedClassObject;
 type ClassName = classes.ClassName;
 
-function each<V>(obj: Record<string, V>, cb: (val: V, key: string) => void): void;
-function each<V, R>(obj: Record<string, V>, cb: (val: V, key: string, out: R) => R, out: R): R;
-function each<V, R>(obj: Record<string, V>, cb: (val: V, key: string, out?: R) => R | undefined, out?: R): R | void {
-  for (const key in obj) {
-    if ({}.hasOwnProperty.call(obj, key)) {
-      out = cb(obj[key], key, out);
+function each<V>(
+  base: Record<string, V>,
+  callback: (value: V, key: string) => void
+): void;
+
+function each<V, R>(
+  base: Record<string, V>,
+  callback: (value: V, key: string, output: R) => R,
+  output: R
+): R;
+
+function each<V, R>(
+  base: Record<string, V>,
+  callback: (value: V, key: string, output?: R | undefined) => R | undefined,
+  output?: R,
+): R | undefined {
+  for (const key in base) {
+    if ({}.hasOwnProperty.call(base, key)) {
+      output = callback(
+        base[key],
+        key,
+        output,
+      );
     }
   }
-  return out;
+  return output;
 }
 
-function normString(str: string, val: boolean, out: NormalizedClassObject): NormalizedClassObject {
-  if (str) {
-    const cns = str.split(' ');
-    const { length: len } = cns;
-    for (let i = 0; i < len; i++) {
-      if (cns[i]) {
-        out[cns[i]] = val;
+function normStrings(names: string[], value: boolean, output: NormalizedClassObject): NormalizedClassObject {
+  const { length } = names;
+  for (let i = 0; i < length; i++) {
+    if (names[i]) {
+      output[names[i]] = value;
+    }
+  }
+  return output;
+}
+
+function normArray(array: ArrayLike<ClassName>, output: NormalizedClassObject): NormalizedClassObject {
+
+  const { length } = array;
+
+  for (let i = 0; i < length; i++) {
+
+    const item = array[i];
+
+    if (item && typeof item === 'object') {
+      if (Array.isArray(item)) {
+        normArray(
+          item,
+          output,
+        );
+      } else {
+        each(item, (value, key) => {
+          if (key) {
+            if (typeof value === 'function') {
+              value = (value as IsClassPresent)(
+                each<boolean, NormalizedClassObject>(
+                  output,
+                  (value, key, result) => {
+                    result[key] = value;
+                    return result;
+                  },
+                  {},
+                ),
+                key.split(' '),
+              );
+            }
+            normStrings(
+              key.split(' '),
+              !!value,
+              output,
+            );
+          }
+        });
+      }
+    } else {
+      const names = `${item}`;
+      if (names) {
+        normStrings(
+          names.split(' '),
+          true,
+          output,
+        );
       }
     }
-  }
-  return out;
-}
 
-function normArray(arr: ArrayLike<ClassName>, out: NormalizedClassObject): NormalizedClassObject {
-  const { length: len } = arr;
-  for (let i = 0; i < len; i++) {
-    const value = arr[i];
-    if (Array.isArray(value)) {
-      normArray(value, out);
-    } else if (value && typeof value === 'object') {
-      let current: Record<string, boolean> | undefined;
-      each(value, (value2, key) => {
-        normString(
-          key,
-          !!(
-            typeof value2 !== 'function' ? value2 : (value2 as IsClassPresent)(
-              current || (
-                current = each<boolean, NormalizedClassObject>(out, (value, key, r) => {
-                  r[key] = value;
-                  return r;
-                }, {})
-              ),
-              key ? key.split(' ') : [],
-            )
-          ),
-          out,
-        );
-      });
-    } else {
-      normString(`${value}`, true, out);
-    }
   }
-  return out;
+
+  return output;
+
 }
 
 function classes(...classnames: ClassName[]): string;
@@ -66,7 +103,7 @@ function classes(): string {
       arguments as ArrayLike<ClassName>,
       {},
     ),
-    (incl, cn, res) => !incl ? res : res ? `${res} ${cn}` : cn,
+    (include, name, result) => !include ? result : result ? `${result} ${name}` : name,
     '',
   );
 }
