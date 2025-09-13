3a0e54dd300657b5deb012df4926f76c0b8c928e
Manuel Fernández <manferlo81@gmail.com>
2025-09-14 00:04:08 +0700

    chore: Move core functionality to a dedicated file

--- __test__/tools/sorted.ts
+++ __test__/tools/sorted.ts
@@ -1,6 +1,16 @@
 import originalClassesFunction from '../../src'
+import { classesToArray as originalClassesToArrayFunction } from '../../src/classes'
 
-export function classes(...names: originalClassesFunction.ClassName[]): string {
-  const result = originalClassesFunction(...names)
-  return result ? result.split(' ').sort().join(' ') : ''
+export function classes(...classNames: originalClassesFunction.ClassName[]): string {
+  const className = originalClassesFunction(...classNames)
+  return className ? className.split(' ').sort().join(' ') : ''
+}
+
+export function classesToArray(...classNames: originalClassesFunction.ClassName[]): string[] {
+  const classArray = originalClassesToArrayFunction(...classNames)
+  return classArray.sort()
+}
+
+export function classesToArrayToString(...classNames: originalClassesFunction.ClassName[]): string {
+  return classesToArray(...classNames).join(' ')
 }
--- src/classes.ts
+++ src/classes.ts
@@ -1,112 +1,21 @@
-import type { ClassArray, ClassesState, ClassItem, ClassObject, DeprecatedState, IsClassPresent, ResolveClass } from './types'
+import { processArray } from './core'
+import type { ClassArray } from './types'
 
-function createDeprecatedState(state: ClassesState): DeprecatedState {
-  return Object.entries(state).reduce((output, [cleanClassName, value]) => {
-    if (!value) return output
-    return { ...output, [cleanClassName]: true }
-  }, {})
-}
+function arrayToArray(classItem: ClassArray): string[] {
+  // Create state from class item
+  const state = processArray({}, classItem)
 
-function cleanupClassName(dirtyClassName: string): string[] | undefined {
-  const trimmedClassName = dirtyClassName.trim()
-  if (!trimmedClassName) return
-  return trimmedClassName.split(/\s+/)
-}
-
-function extendStateClean(input: ClassesState, cleanClassNames: string[], value: boolean): ClassesState {
-  return cleanClassNames.reduce((state, cleanClassName) => {
-    return { ...state, [cleanClassName]: value }
-  }, input)
-}
-
-function processFunctionItem(state: ClassesState, resolveClass: ResolveClass): ClassesState {
-  // Create current state
-  const deprecatedState = createDeprecatedState(state)
-
-  // Call resolver to get a class item
-  const classItem = resolveClass(deprecatedState)
-
-  // Process returned class item
-  return processClassItem(state, classItem)
-}
-
-function processObjectItem(state: ClassesState, classObject: ClassObject): ClassesState {
-  // FIXME: Optimize
-  return Object.entries(classObject).reduce((_state, [key, value]) => {
-    const cleanClassNames = cleanupClassName(key)
-    if (!cleanClassNames) return _state
-
-    // apply changes based on value if value is not a function
-    if (typeof value !== 'function') {
-      return extendStateClean(
-        _state,
-        cleanClassNames,
-        !!value,
-      )
-    }
-
-    // create current state
-    const deprecatedState = createDeprecatedState(_state)
-
-    // call value as function
-    const isPresent = (value as IsClassPresent)(
-      deprecatedState,
-      [...cleanClassNames],
-    )
-
-    // Extend state with clean classnames
-    return extendStateClean(
-      _state,
-      cleanClassNames,
-      !!isPresent,
-    )
-  }, state)
-}
-
-function processClassItem(state: ClassesState, item: ClassItem): ClassesState {
-  // Skip if item is nullish
-  if (item == null) return state
-
-  // Process item if it's a function
-  if (typeof item === 'function') return processFunctionItem(state, item)
-
-  // Process item if it's an array
-  if (Array.isArray(item)) return processArray(state, item)
-
-  // Process item if it's an object
-  if (typeof item === 'object') return processObjectItem(state, item as ClassObject)
-
-  // Convert user item to string
-  // It is considered "dirty", so it has to be cleaned up
-  const dirtyClassName = `${item as unknown}`
-  const cleanClassNames = cleanupClassName(dirtyClassName)
-
-  // Skip item if item couldn't be cleaned up
-  if (!cleanClassNames) return state
-
-  // Extend state with clean classnames
-  return extendStateClean(
-    state,
-    cleanClassNames,
-    true,
-  )
-}
-
-function processArray(state: ClassesState, classNames: ClassArray): ClassesState {
-  return classNames.reduce(processClassItem, state)
-}
-
-function stateToArray(state: ClassesState): string[] {
+  // Get clean classnames from state
   const classNames = Object.keys(state)
+
+  // Return filtered classname
   return classNames.filter((className) => state[className])
 }
 
-function stringifyState(state: ClassesState): string {
-  const filteredClassNames = stateToArray(state)
-  return filteredClassNames.join(' ')
+export function classesToArray(...classNames: ClassArray): string[] {
+  return arrayToArray(classNames)
 }
 
 export function classes(...classNames: ClassArray): string {
-  const state = processArray({}, classNames)
-  return stringifyState(state)
+  return arrayToArray(classNames).join(' ')
 }
