58e23c2f7d92e85d5211754f530a812086c27315
Manuel Fernández <manferlo81@gmail.com>
2025-09-19 16:30:53 +0700

    chore: Move tools to dedicated file

--- src/core.ts
+++ src/core.ts
@@ -1,38 +1,6 @@
 import type { IsArrayFunction } from './private-types'
-import type { ClassesState, ClassItem, DeprecatedState, IsClassPresent } from './types'
-
-// DO NOT OPTIMIZE!!
-// It will be removed in the future...
-// ...as functions will receive the whole state
-//
-// TODO: Implement function calls with whole state
-function deprecated_createState(state: ClassesState): DeprecatedState {
-  return Object.keys(state).reduce((output, cleanClassName) => {
-    if (!state[cleanClassName]) return output
-    return { ...output, [cleanClassName]: true }
-  }, {})
-}
-
-// Core functionalities
-
-/**
- * Converts a "dirty" class name into an array of "clean" class names.
- * A "dirty" class name may content multiple class names and unwanted spaces.
- * A "clean" class name is a single class name without any space
- */
-function cleanupClassName(dirtyClassName: string): string[] {
-  return dirtyClassName.split(/\s+/).filter(Boolean)
-}
-
-/**
- * Extends the current state with the provided clean classnames and value
- */
-function extendStateClean(state: ClassesState, cleanClassNameList: string[], value: boolean): ClassesState {
-  return cleanClassNameList.reduce(
-    (state, cleanClassName) => ({ ...state, [cleanClassName]: value }),
-    state,
-  )
-}
+import { cleanupClassName, deprecated_createState, extendStateClean } from './tools'
+import type { ClassesState, ClassItem, IsClassPresent } from './types'
 
 /**
  * Processes a class item.
